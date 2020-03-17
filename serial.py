9c85ece49abe7053df5fab54600df4242670e9f7
Max Jung <mjung@Peters-MacBook-Pro.local>
2020-03-17 15:40:11 -0700

    serial.py commit

---
+++ serial.py
@@ -0,0 +1,35 @@
+"""Python serial number generator."""
+
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+
+    def __init__(self, start):
+        "Initial serial number"
+        self.start = start
+        self.initial = start
+
+    def generate(self):
+        "Generates new serial number (+1 of original start_number)"
+        print(self.start)
+        self.start += 1
+
+    def reset(self):
+        self.start = self.initial
